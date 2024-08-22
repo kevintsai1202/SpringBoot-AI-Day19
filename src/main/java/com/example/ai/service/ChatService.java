a434ec92cb03b1215f78076fc6e857a655c799d3
kevintsai <kevintsai@TD-ID10-01460.taidoc.com.tw>
2024-08-22 16:52:44 +0800

    update

--- src/main/java/com/example/ai/service/ChatService.java
+++ src/main/java/com/example/ai/service/ChatService.java
@@ -1,26 +1,29 @@
 package com.example.ai.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
-import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class ChatService {
 	private final ChatClient chatClient;
-	private ChatMemory chatMemory = new InMemoryChatMemory();
+	private final ChatMemory chatMemory = new InMemoryChatMemory();
 	
-	public String chat(String chatId, String userMessage) {
-		chatMemory.add(chatId, new UserMessage(userMessage));
-	   
-		return this.chatClient.prompt()
-	            .messages(chatMemory.get(chatId, 2))
-	            .call()
-	            .content();
+	public String chat(String chatId, String userMessage) {	   
+		log.info("Chat ID:{} User Message:{}",chatId,userMessage);
+		String assistantMessage = this.chatClient.prompt()
+		.advisors(new MessageChatMemoryAdvisor(chatMemory, chatId, 30))
+		.user(userMessage)
+        .call().content();
+		log.info("Chat ID:{} Assistant Message:{}",chatId,assistantMessage);
+		return assistantMessage;
 	}
 }
